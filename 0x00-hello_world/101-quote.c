e219540958e26ee9fdaa4465d74f86804b1d14a4
KambaleNdulani <sagessendulani@gmail.com>
2022-12-12 08:09:45 -0800

    fixing the bety coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,12 @@
 #include <unistd.h>
 
-
-
 /**
+ * main - Entry point of the program
  *
- *  * main - write to stdout
  *
- *   * Return: Always 0
- *
- *    */
-
+*/
 int main(void)
-
 {
-
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+	write(1, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+		return (1);
 }
